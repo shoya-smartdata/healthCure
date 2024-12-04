c3decb333ba47aad6edfd7b808075afb0c3e1ea7
mrshoya05 <shoyabkhan0508@gmail.com>
2024-12-04 14:57:44 +0530

    .

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    optimizeDeps: {
-        include: ["swiper"],
-      },
-      
+  base: '/', // Ensure base path is correct
+  optimizeDeps: {
+    include: ["swiper"], // Include any dependencies that require pre-bundling
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build files
+  },
 });
