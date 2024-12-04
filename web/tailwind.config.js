3766c9d483b04deead37892b32a5784d0c7a225d
mrshoya05 <shoyabkhan0508@gmail.com>
2024-12-04 15:06:41 +0530

    .

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}', // Adjust to your file extensions
+    './index.html', // Add paths to your HTML and template files
+    './src/**/*.{vue,js,ts,jsx,tsx}', // Adjust based on your project structure
   ],
   theme: {
     extend: {},
