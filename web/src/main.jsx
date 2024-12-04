0305c4b2382783a7dc40fc6047292a3bb90f48e6
mrshoya05 <shoyabkhan0508@gmail.com>
2024-12-04 15:04:23 +0530

    build

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';  // Important: Import from 'react-dom/c
 import './index.css'
 import App from './App';
 import { ThemeProvider } from './middleware/ThemeContext';
+import './assets/tailwind.css';
+
 
 // Create the root and render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",   // Ensure Tailwind processes your HTML files
-    "./src/**/*.{js,jsx,ts,tsx}",  // Process JSX/TSX files in your src folder
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}', // Adjust to your file extensions
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
