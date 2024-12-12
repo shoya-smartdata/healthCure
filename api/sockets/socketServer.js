d7caa055e5957b714b77f19f6a4a6e5df015906c
mrshoya05 <shoyabkhan0508@gmail.com>
2024-12-12 18:19:05 +0530

    55

--- api/sockets/socketServer.js
+++ api/sockets/socketServer.js
@@ -12,16 +12,13 @@ const initSocketServer = (server) => {
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    // Join a chat room
+
     socket.on("joinRoom", ({ roomId }) => {
-      socket.join(roomId); // Join a specific room
+      socket.join(roomId); 
       console.log(`User ${socket.id} joined room ${roomId}`);
     });
-
-    // Handle incoming chat messages
     socket.on("chatMessage", async ({ roomId, senderId, message }) => {
       try {
-        // Save the message to the database
         const newMessage = await ChatMessage.create({
           roomId,
           senderId,
